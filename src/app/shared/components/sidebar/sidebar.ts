4477496304399c34b55855f2fc9dc86547a2ac57
Yaswitha Sakhamuri <yaswitha@example.com>
2026-01-02 07:15:21 +0530

    update sidebar

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -8,22 +8,23 @@ import { AuthService } from '../../../core/services/auth';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css'
 })
 export class SidebarComponent implements OnInit {
   isAdmin = false;
   username = 'User';
-
+  isConsumer = false;
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     const user = this.authService.getUserFromStorage();
-    if (user) {
+    if (user && user.roles) {
       this.username = user.username;
-      this.isAdmin = user.roles && user.roles.includes('ROLE_ADMIN');
+      this.isAdmin = user.roles.includes('ROLE_ADMIN') || user.roles.includes('ROLE_BILLING_OFFICER');
+      this.isConsumer = user.roles.includes('ROLE_CONSUMER');
     }
   }
-
-  logout() {
+  onLogout() {
     this.authService.logout();
   }
 }
